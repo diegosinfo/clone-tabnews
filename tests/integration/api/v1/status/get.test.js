cbbe9de3aa8de103af6e8c4f9aa3f384c4545f7e
Diego O <doliveira.silva28@gmail.com>
2025-03-13 02:11:34 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
+const { interpolateAs } = require("next/dist/shared/lib/router/router");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toBe(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  expect(parseInt(responseBody.dependencies.database.max_connection)).toBe(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+
+  const openedConnection = parseInt(
+    responseBody.dependencies.database.opened_connections,
+  );
+
+  expect(
+    parseInt(responseBody.dependencies.database.opened_connections),
+  ).toEqual(1);
 });
